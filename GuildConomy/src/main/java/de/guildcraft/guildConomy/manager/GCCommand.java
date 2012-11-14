d1b5d5dfc1b07df335a872441a199cc06b94f781
sirkarpfen <Die Scholzis@DieScholzis-PC.localdomain>
2012-11-14 22:14:33 +0200

    first commit

---
+++ GuildConomy/src/main/java/de/guildcraft/guildConomy/manager/GCCommand.java
@@ -0,0 +1,51 @@
+package de.guildcraft.guildConomy.manager;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import de.guildcraft.guildConomy.GCPlugin;
+
+public class GCCommand implements CommandExecutor{
+	
+	private final Map<String, GCSubcommand> subcommands = new HashMap<String, GCSubcommand> ();
+	
+	public GCCommand(GCPlugin plugin) {
+		subcommands.put("", new GCHelpCommand(plugin));
+		subcommands.put("set", new GCSetCommand(plugin));
+		subcommands.put("delete", new GCDeleteCommand(plugin));
+		subcommands.put("pay", new GCPayCommand(plugin));
+		subcommands.put("balance", new GCBalanceCommand(plugin));
+	}
+	
+	// ---------------------------------------------------------------------------------------------
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length > 0) {
+			for (Entry<String, GCSubcommand> entry : subcommands.entrySet()) {
+				if (entry.getKey().equals(args[0])) {
+					return entry.getValue().permissionExecute(sender, getSubArgs(args));
+				}
+			}
+		}
+		sender.sendMessage(ChatColor.RED + "Der Befehl wurde leider nicht gefunden");
+		return false;
+	}
+
+	// ---------------------------------------------------------------------------------------------
+
+	private String[] getSubArgs(String[] args) {
+		String[] subArgs = new String[args.length - 1];
+		for (int i = 1; i < args.length; ++i)
+			subArgs[i - 1] = args[i];
+		return subArgs;
+	}
+
+	// ---------------------------------------------------------------------------------------------
+	
+}
